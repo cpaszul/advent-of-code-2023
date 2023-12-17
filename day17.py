171e546be1894dec9aa4888ebe2bb2336b51ee22
Cory Paszul <cpaszul@gmail.com>
2023-12-17 18:53:14 -0500

    Improved speed of day 17

--- day17.py
+++ day17.py
@@ -14,13 +14,19 @@ def part_1(loc: str = DEFAULT_INPUT) -> int:
     h = [start]
     while h:
         heat, x, y, px, py = heapq.heappop(h)
-        state = (x, y, px, py)
+        if x == px and y == py:
+            prev = (0, 0)
+        elif x == px:
+            prev = (0, -1 if py < y else 1)
+        elif y == py:
+            prev = (-1 if px < x else 1, 0)
+        state = (x, y, prev)
         if state in seen:
             continue
         seen.add(state)
         if (x, y) == (max_x - 1, max_y - 1):
             return heat
-        for neigh in neighbors(grid, (x, y), (px, py)):
+        for neigh in neighbors(grid, (x, y), prev):
             node, cost = neigh
             heapq.heappush(h, (heat + cost, *node, x, y))
 
@@ -30,7 +36,7 @@ def neighbors(grid: list[list[int]], node: Point, prev: Point) -> list[tuple[Poi
     max_y = len(grid)
     max_x = len(grid[0])
     neighs = []
-    if x == px:
+    if px == 0:
         for i in range(1, 4):
             if 0 <= x + i < max_x:
                 cost = sum(grid[y][x + k] for k in range(1, i + 1))
@@ -39,7 +45,7 @@ def neighbors(grid: list[list[int]], node: Point, prev: Point) -> list[tuple[Poi
             if 0 <= x + i < max_x:
                 cost = sum(grid[y][x + k] for k in range(-1, i - 1, -1))
                 neighs.append(((x + i, y), cost))
-    if y == py:
+    if py == 0:
         for i in range(1, 4):
             if 0 <= y + i < max_y:
                 cost = sum(grid[y + k][x] for k in range(1, i + 1))
@@ -60,13 +66,19 @@ def part_2(loc: str = DEFAULT_INPUT) -> int:
     h = [start]
     while h:
         heat, x, y, px, py = heapq.heappop(h)
-        state = (x, y, px, py)
+        if x == px and y == py:
+            prev = (0, 0)
+        elif x == px:
+            prev = (0, -1 if py < y else 1)
+        elif y == py:
+            prev = (-1 if px < x else 1, 0)
+        state = (x, y, prev)
         if state in seen:
             continue
         seen.add(state)
         if (x, y) == (max_x - 1, max_y - 1):
             return heat
-        for neigh in neighbors_p2(grid, (x, y), (px, py)):
+        for neigh in neighbors_p2(grid, (x, y), prev):
             node, cost = neigh
             heapq.heappush(h, (heat + cost, *node, x, y))
 
@@ -76,7 +88,7 @@ def neighbors_p2(grid: list[list[int]], node: Point, prev: Point) -> list[tuple[
     max_y = len(grid)
     max_x = len(grid[0])
     neighs = []
-    if x == px:
+    if px == 0:
         for i in range(4, 11):
             if 0 <= x + i < max_x:
                 cost = sum(grid[y][x + k] for k in range(1, i + 1))
@@ -85,7 +97,7 @@ def neighbors_p2(grid: list[list[int]], node: Point, prev: Point) -> list[tuple[
             if 0 <= x + i < max_x:
                 cost = sum(grid[y][x + k] for k in range(-1, i - 1, -1))
                 neighs.append(((x + i, y), cost))
-    if y == py:
+    if py == 0:
         for i in range(4, 11):
             if 0 <= y + i < max_y:
                 cost = sum(grid[y + k][x] for k in range(1, i + 1))
